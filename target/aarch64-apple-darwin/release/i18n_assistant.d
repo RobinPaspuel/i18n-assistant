7b745e009376af8f316dad7b131aece5cc37cae3
Robin Paspuel <robinson.paspuel@alegra.com>
2024-10-21 08:40:59 -0500

    add aarch64-apple-darwin build

---
+++ target/aarch64-apple-darwin/release/i18n_assistant.d
@@ -0,0 +1,1 @@
+/Users/RobinAlegra/Desktop/personal_projects/i18n-assistant/target/aarch64-apple-darwin/release/i18n_assistant: /Users/RobinAlegra/Desktop/personal_projects/i18n-assistant/src/main.rs
