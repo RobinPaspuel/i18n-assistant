e762787bc5580917776a3d766002b6c3cff6b66f
Robin Paspuel <robinson.paspuel@alegra.com>
2024-10-21 14:33:21 -0500

    add gitignore

--- target/aarch64-apple-darwin/release/deps/i18n_assistant-d47aa39938345603.d
+++
@@ -1,5 +0,0 @@
-/Users/RobinAlegra/Desktop/personal_projects/i18n-assistant/target/aarch64-apple-darwin/release/deps/i18n_assistant-d47aa39938345603: src/main.rs
-
-/Users/RobinAlegra/Desktop/personal_projects/i18n-assistant/target/aarch64-apple-darwin/release/deps/i18n_assistant-d47aa39938345603.d: src/main.rs
-
-src/main.rs:
--- target/aarch64-apple-darwin/release/i18n_assistant.d
+++
@@ -1,1 +0,0 @@
-/Users/RobinAlegra/Desktop/personal_projects/i18n-assistant/target/aarch64-apple-darwin/release/i18n_assistant: /Users/RobinAlegra/Desktop/personal_projects/i18n-assistant/src/main.rs
